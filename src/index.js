7118164777c6004c1260ab88b041e2d6d523eddb
jjtsou <itsourtis@gmail.com>
2023-09-16 21:34:58 +0300

    delete old code

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { nowPlayingMovies, storeMovieGenres } from "./js/Utils.js";
-import { infiniteScroll, searchDelay } from "./js/UI-Handlers.js";
-
-window.addEventListener('load',storeMovieGenres);
-window.addEventListener('load',nowPlayingMovies);
-window.addEventListener('scroll', infiniteScroll);
-
-const searchButton = document.getElementById('searchForMovies');
-searchButton.addEventListener('keyup',searchDelay);
